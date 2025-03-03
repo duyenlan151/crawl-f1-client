0b367d94c38907c46dcb0a6d7a08b94678a72fb3
duyenlan151 <1660144.16ck1@gmail.com>
2025-03-03 22:44:57 +0700

    feat: integrate API for F1 crawler

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,15 @@ import './index.css';
 import QueryProvider from './provider/query-provider.tsx';
 import Routes from './routes';
 import { ToasterConfig } from '@/components';
+import { F1DataProvider } from '@/context/F1DataContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryProvider>
       <ToasterConfig />
-      <Routes />
+      <F1DataProvider>
+        <Routes />
+      </F1DataProvider>
     </QueryProvider>
   </React.StrictMode>,
 );
